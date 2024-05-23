ba656081cb50ac555a0131fc353104bc0f86bd07
Hunter Wei <hunterwei@gmail.com>
2024-05-22 23:28:03 -0500

    Changed to Json2queue

---
+++ ImageFunctions/Json2queue.cs
@@ -0,0 +1,59 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+// Learn how to locally debug an Event Grid-triggered function:
+//    https://aka.ms/AA30pjh
+
+// Use for local testing:
+//   https://{ID}.ngrok.io/runtime/webhooks/EventGrid?functionName=Thumbnail
+
+using Azure.Messaging.EventGrid;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImageFunctions
+{
+    public static class Json2queue
+    {
+        private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        private static string GetBlobNameFromUrl(string bloblUrl)
+        {
+            var uri = new Uri(bloblUrl);
+            var blobClient = new BlobClient(uri);
+            return blobClient.Name;
+        }
+
+        [FunctionName("Json2queue")]
+        public static async Task Run(
+            [EventGridTrigger]EventGridEvent eventGridEvent,
+            [Blob("{data.url}", FileAccess.Read)] Stream input,
+            ILogger log)
+        {
+            try
+            {
+                if (input != null)
+                {
+                    var serializer = new JsonSerializer();
+
+                    using (var sr = new StreamReader(input))
+                    using (var jsonTextReader = new JsonTextReader(sr))
+                    {
+                        var jsObj = serializer.Deserialize(jsonTextReader);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+                throw;
+            }
+        }
+    }
+}
